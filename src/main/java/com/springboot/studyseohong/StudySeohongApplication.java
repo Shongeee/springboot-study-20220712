bad38e7381241d8b75351f396b5381981abb741c
Shongeee <hongeever@gmail.com>
2022-07-12 20:53:13 +0900

    init commit

---
+++ src/main/java/com/springboot/studyseohong/StudySeohongApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyseohong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySeohongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySeohongApplication.class, args);
+	}
+
+}
